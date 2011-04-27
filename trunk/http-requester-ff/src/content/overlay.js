ab80c20e90cb8cdacff3c37655a422e98649db8e
tommut <tommut@3ed6b9a9-2bbf-3b19-85c0-b63fae505c5e>
2011-04-27 21:19:54 +0000

    httprequester-ize'd

--- trunk/http-requester-ff/src/content/overlay.js
+++ trunk/http-requester-ff/src/content/overlay.js
@@ -1,7 +1,9 @@
-function showPoster() {
+if(!httprequester) var httprequester={};
+
+httprequester.showHttpRequester = function() {
    var windowManager = Components.classes['@mozilla.org/appshell/window-mediator;1'].getService();
    var windowManagerInterface = windowManager.QueryInterface(Components.interfaces.nsIWindowMediator);
-   var topWindow = windowManagerInterface.getMostRecentWindow("window:poster");
+   var topWindow = windowManagerInterface.getMostRecentWindow("window:httprequester");
    if (topWindow) {
    		try { 
     		 topWindow.focus();
@@ -11,7 +13,7 @@ function showPoster() {
    }
    else {
 	   window.openDialog(
-	      'chrome://poster/content/poster-window.xul','poster-'+(new Date()).getTime(),'chrome,centerscreen,resizable,dialog=no'
+	      'chrome://httprequester/content/httprequester-window.xul','httprequester-'+(new Date()).getTime(),'chrome,centerscreen,resizable,dialog=no'
 	   );
    	}
    
